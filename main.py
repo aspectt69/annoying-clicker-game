cad80bf9d6952a75da25b4a5548536d1db82121b
aspectt69 <jaylengordon2604@gmail.com>
2024-12-01 16:54:57 +1100

    upgrade now works

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pgzrun
+import pygame
 import os
 import random
 
@@ -8,6 +9,10 @@ HEIGHT = 600
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
 clicks = 0
+upgrade1cost = 1
+secondsplaying = 0
+minutesplaying = 0
+hoursplaying = 0
 floating_scores = []
 
 button = Actor("redbutton")
@@ -17,6 +22,10 @@ button.multiplier = 1
 redarrow = Actor("redarrow")
 redarrow.x = (WIDTH // 2)
 redarrow.y = (HEIGHT // 2.5)
+
+upgradebutton1 = Actor("upgradebutton1")
+upgradebutton1.x = 95
+upgradebutton1.y = 85
 
 class Floating_score():
     def __init__(self, text, x, y, size, color=(0,0,0), duration=30, offset=5):
@@ -46,22 +55,53 @@ class Floating_score():
         screen.draw.text(self.text, (self.x - len(self.text), self.y), color=self.color, fontsize=50, alpha=self.alpha, owidth=0.5, ocolor="white")
 
 
+
+
 def on_mouse_down(pos):
     global clicks
+    global upgrade1cost
     if button.collidepoint(pos):
         if random.randint(1, 15) != 15:
             print("Clicked button")
             clicks += 1 * button.multiplier
-            floating_scores.append(Floating_score(f"+{1 * button.multiplier}", pos[0], pos[1], 1))
+            floating_scores.append(Floating_score(f"+{round(1 * button.multiplier, 1)}", pos[0], pos[1], 1))
         else:
             print("Click missed")
             floating_scores.append(Floating_score("Oops", pos[0], pos[1], 2, (255, 0, 0), 45, 7))
+    if upgradebutton1.collidepoint(pos):
+        if clicks >= upgrade1cost:
+            if random.randint(1, 10) != 10:
+                print("Upgraded")
+                button.multiplier = button.multiplier + button.multiplier * 0.15
+                clicks = clicks - upgrade1cost
+                upgrade1cost = upgrade1cost * 1.25
+            else:
+                print("Upgrade Moved")
+                upgradebutton1.x = random.randint(95, 595)
+                upgradebutton1.y = random.randint(85, 110)
+        else:
+            print("Not enough to upgrade")
 
 def draw():
     screen.clear()
     screen.fill((255,255,255))
-    screen.draw.text("Clicks: " + str(clicks), (20, 20), color = (0,0,0), fontsize = 50)
-    screen.draw.text("Click here by the way", (redarrow.x - redarrow.width // 2 - 55, redarrow.y - redarrow.height // 2 - 25), color = (0,0,0,), fontsize = 30)
+    screen.draw.text("Clicks: " + str(round(clicks)), (20, 20), color = (0,0,0), fontsize = 50)
+    screen.draw.text("Click here by the way", (redarrow.x - redarrow.width // 2 - 55, redarrow.y - redarrow.height // 2 - 25), color=(0,0,0,), fontsize=30)
+    screen.draw.text("Random events happen", (20, 470), color=(0,0,0), fontsize=25)
+    screen.draw.text("every 30 seconds!", (20, 490), color=(0,0,0), fontsize=25)
+    screen.draw.text("Time spent playing:", (20, 520), color=(0,0,0), fontsize=20)
+    screen.draw.text("Hours: " + str(hoursplaying), (20, 540), color=(0,0,0), fontsize=20)
+    screen.draw.text("Minutes: " + str(minutesplaying), (20, 560), color=(0,0,0), fontsize=20)
+    screen.draw.text("Seconds: " + str(round(secondsplaying)), (20, 580), color=(0,0,0), fontsize=20)
+
+    upgradebutton1.draw()
+    upgrade1_text = f"Upgrade: {round(upgrade1cost)}"
+    font = pygame.font.Font(None, 30)
+    text_width, text_height = font.size(upgrade1_text)
+    text_x = upgradebutton1.x - (text_width // 2)
+    text_y = upgradebutton1.y - (text_height // 2)
+    screen.draw.text(upgrade1_text, (text_x, text_y), color=(0, 0, 0), fontsize=30, ocolor="white", owidth=0.5)
+
     redarrow.draw()
     button.draw()
 
@@ -71,6 +111,13 @@ def draw():
             floating_scores.remove(score)
 
 def update():
-    pass
+    global secondsplaying, minutesplaying, hoursplaying
+    secondsplaying += 1 / 60
+    if secondsplaying == 60:
+        secondsplaying = 0
+        minutesplaying += 1
+    elif minutesplaying == 60:
+        minutesplaying = 0
+        hoursplaying += 1
 
 pgzrun.go()
